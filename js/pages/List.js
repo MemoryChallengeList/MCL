dee9e4f2c231907f9c989b57166735316ffdf36d
SkithyYT <luca.skithy@gmail.com>
2026-01-29 23:56:24 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,7 +100,7 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps if higher run is not accepted)
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
@@ -112,7 +112,7 @@ export default {
                         The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        The recording must also show the endscreen, or the completion will be invalidated.
                     </p>
                     <p>
                         Do not use secret routes or bug routes
@@ -123,6 +123,14 @@ export default {
                     <p>
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
                     </p>
+                    <p>
+                        Minimal rng
+                    </p>
+                    <p>
+                       MUST BE 30 SECONDS OR BELOW
+                    </p>
+                    <p>
+                      No decoration (yet) and you must make it yourself
                 </div>
             </div>
         </main>
